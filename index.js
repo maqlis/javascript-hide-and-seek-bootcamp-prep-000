73ed9bf5c0f60697347d5bced8b95da4f4448a0e
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-25 12:35:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ function nestedTarget(){
 }
 function deepestChild(){
   var lista = document.querySelectorAll("#grand-node div")
-  for (var i = 0; i<lista.length; i++){
-
-  }
   return lista[lista.length - 1]
 }
 function increaseRankBy(n){
-  
+
 }
